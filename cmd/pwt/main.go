a4e9189b44682cd720baa9674b744a2106f76e5a
Simon Mittag <simonmittag@gmail.com>
2020-05-13 09:53:02 +1000

    new args

--- cmd/pwt/main.go
+++ cmd/pwt/main.go
@@ -2,20 +2,39 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/simonmittag/pwt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	host := flag.String("t", "localhost", "the destination host name")
-	port := flag.Uint("p", 80, "the destination port")
+	host := "localhost"
+	port := 80
 	timeSeconds := flag.Int("w", 10, "time wait in seconds")
+	modeVersion := flag.Bool("v", false, "print pwt version")
 	flag.Parse()
 
-	ok := pwt.Zzz(*host, uint16(*port), *timeSeconds)
-	if ok {
-		os.Exit(0)
+	args := flag.Args()
+	if len(args) == 1 {
+		if strings.Contains(args[0], ":") {
+			ph := strings.Split(args[0], ":")
+			host = ph[0]
+			port, _ = strconv.Atoi(ph[1])
+		} else {
+			host = args[0]
+		}
+	}
+
+	if *modeVersion {
+		fmt.Printf("pwt[%s]\n", pwt.Version)
 	} else {
-		os.Exit(-1)
+		ok := pwt.Zzz(host, uint16(port), *timeSeconds)
+		if ok {
+			os.Exit(0)
+		} else {
+			os.Exit(-1)
+		}
 	}
 }
