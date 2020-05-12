b9265003f8de2e5b680776f042c449bc33f38ac8
Simon Mittag <simonmittag@gmail.com>
2020-05-13 05:50:44 +1000

    logging

--- pwt.go
+++ pwt.go
@@ -10,18 +10,18 @@ import (
 const Version string = "v0.1.0"
 
 func Zzz(host string, port uint16, timeSeconds int) {
-	fmt.Printf("pwt[%s] attempting connection to %s:%d", Version, host, port)
+	fmt.Printf("pwt[%s] attempting to dial %s on %d ", Version, host, port)
 	for i := 0; i < timeSeconds; i++ {
 		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
 		if err != nil {
 			time.Sleep(time.Duration(1 * time.Second))
 			fmt.Print(".")
 		} else {
-			fmt.Printf("pwt[%s] %s:%d connected after %d seconds", Version, host, port, i)
+			fmt.Printf("\npwt[%s] succesfully connected to %s:%d after %d seconds\n", Version, host, port, i)
 			conn.Close()
 			os.Exit(0)
 		}
 	}
-	fmt.Printf("pwt[%s] %s:%d not connected after %d seconds, aborting...", Version, host, port, timeSeconds)
+	fmt.Printf("\npwt[%s] aborting attempted dial to %s on %d after %d seconds, exiting\n", Version, host, port, timeSeconds)
 	os.Exit(-1)
 }
