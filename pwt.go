ca6ad96c47defd6774582fec5b12f204eae56089
Simon Mittag <simonmittag@gmail.com>
2020-05-13 05:44:12 +1000

    pwt

--- pwt.go
+++ pwt.go
@@ -5,22 +5,23 @@ import (
 	"net"
 	"os"
 	"time"
-	"github.com/rs/zerolog/log"
 )
 
 const Version string = "v0.1.0"
 
 func Zzz(host string, port uint16, timeSeconds int) {
-	for i:=0;i<timeSeconds;i++{
+	fmt.Printf("pwt[%s] attempting connection to %s:%d", Version, host, port)
+	for i := 0; i < timeSeconds; i++ {
 		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
-		defer conn.Close()
 		if err != nil {
-			time.Sleep(time.Duration(1*time.Second))
+			time.Sleep(time.Duration(1 * time.Second))
+			fmt.Print(".")
 		} else {
-			log.Info().Msgf("pwt[%s] %s:%d connected after %d seconds", Version, host, port, i)
+			fmt.Printf("pwt[%s] %s:%d connected after %d seconds", Version, host, port, i)
+			conn.Close()
 			os.Exit(0)
 		}
 	}
-	log.Info().Msgf("pwt[%s] %s:%d not connected after %d seconds, aborting...", Version, host, port, timeSeconds)
+	fmt.Printf("pwt[%s] %s:%d not connected after %d seconds, aborting...", Version, host, port, timeSeconds)
 	os.Exit(-1)
 }
